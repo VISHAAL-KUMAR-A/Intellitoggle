09cee2afe18ae65e36f309985b24005922e2ecc8
VISHAAL-KUMAR-A <vkthedon123@gmail.com>
2025-10-16 23:02:50 +0530

    first commit

---
+++ frontend-assignment/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="min-height: 100vh; background-color: white; margin: 0; padding: 0;">
+    <NuxtRouteAnnouncer />
+    <AppHeader />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
---
+++ frontend-assignment/components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header style="position: sticky; top: 0; z-index: 50; width: 100%; background-color: #42389E; margin: 0; padding: 0;">
+    <div style="height: 60px;">
+    </div>
+  </header>
+</template>
